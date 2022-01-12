9d50515855505bcd6e87e0ff75c8f620edfcfcf3
MDemp <mdempsey.cbc.ut@gmail.com>
2022-01-11 18:30:48 -0600

    Added notes and psuedocode

---
+++ script.js
@@ -0,0 +1,8 @@
+// Referrence moment.js to get current date and display as text on html
+
+// Create click event on save button that takes the value of text area and saves it to (localStorage.setItem)
+
+// Grab values from local storage using key and then set as value of set area (localStorage.getItem)
+
+// Create conditional to change color of note blocks based on time of day
+
