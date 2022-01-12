73c920c9f78cde5c72cf7f3f2681f26f7ba0fca2
MDemp <mdempsey.cbc.ut@gmail.com>
2022-01-11 19:14:41 -0600

    localStorange and getItem

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Referrence moment.js to get current date and display as text on html
+$("#currentDay").text(moment().format("MMMM DD, YYYY"))
 
 // Create click event on save button that takes the value of text area and saves it to (localStorage.setItem)
+$(".saveBtn").on("click", function(){
+    var noteValue = $(this).siblings(".description").val()
+    var noteKey = $(this).parent().attr("id")
 
+    localStorage.setItem(noteKey, noteValue)
+})
 // Grab values from local storage using key and then set as value of set area (localStorage.getItem)
+$("#9 .description").val(localStorage.getItem("9"))
+$("#10 .description").val(localStorage.getItem("10"))
+
 
 // Create conditional to change color of note blocks based on time of day
 
